99091102117c718632ffdbb63270cc9886992444
OverTimeChaos <jamesfan2006@hotmail.com>
2023-11-09 10:06:24 +1100

    Added user classes and began making log on function

--- bank.py
+++ bank.py
@@ -1,1 +1,21 @@
-print ("hello world")
+class User:
+    def __init__ (self,name):
+        # the User name and age of account 
+        self.name = name
+    def displaydetails(self):
+        #displays details of account
+        print (self.name)
+        print (self.age)
+class Aidan(User):
+    def __init__(self,name):
+        super().__init__(name)
+        self.age = -136
+class Akhil(User):
+    def __init__(self,name):
+        super().__init__(name)
+        self.age = 17
+class Adam(User):
+    def __init__(self,name):
+        super().__init__(name)
+        self.age = 2
+def 
