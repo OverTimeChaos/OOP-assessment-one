d13dda952f10867ae175727c3b70d32773e9a9a6
OverTimeChaos <jamesfan2006@hotmail.com>
2023-11-09 17:43:52 +1100

    Removed redundant classes

--- bank.py
+++ bank.py
@@ -1,21 +1,16 @@
 class User:
-    def __init__ (self,name):
+    def __init__ (self,name,age):
         # the User name and age of account 
         self.name = name
+        self.age = age
     def displaydetails(self):
         #displays details of account
         print (self.name)
         print (self.age)
-class Aidan(User):
-    def __init__(self,name):
-        super().__init__(name)
-        self.age = -136
-class Akhil(User):
-    def __init__(self,name):
-        super().__init__(name)
-        self.age = 17
-class Adam(User):
-    def __init__(self,name):
-        super().__init__(name)
-        self.age = 2
-def 
+def logon(x):
+    if lockingout == 3:
+        print ("Failed too many times. You have been locked out.")
+        quit()
+name = input ("please enter a name ")
+lockingout = 0
+logon(name)
