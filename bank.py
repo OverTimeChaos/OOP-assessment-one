77b974908bb823a1257aefe5f0efa7ee603c5c41
OverTimeChaos <jamesfan2006@hotmail.com>
2023-11-14 10:38:39 +1100

    Finished deposit methord and finsihed UI function

--- bank.py
+++ bank.py
@@ -5,27 +5,40 @@ class User:
         self.age = age
     def displaydetails(self):
         #displays details of account
-        print (self.name)
-        print (self.age)
+        print ("Name: " + self.name)
+        print ("Age: " + self.age)
 class Bank (User):
     def __init__(self,name,age):
-        super().__init__ (self,name,age)
+        super().__init__ (name,age)
         self.balance = 0 
     def deposit (self,amount):
         self.balance = self.balance + amount
         print (f"New balance is {self.balance}")
 def ui (x,age):
-    print (f'''Enter:
-           {\033[1m}bank{"\033[0m}
+    print ('''Enter:
+        \033[1m   Account \033[0m
+           Details - TO display account details
+        \033[1m   Bank \033[0m
            Deposit - To deposit money into bank account
            Withdraw - To withdraw money from bank account
            Display - To display details of your bank account
-           
-           
-''')
-    option = input("What do you want to do, " + x )
-    if option.lower() == "deposit":
-        pass
+        \033[1m   Shopping \033[0m
+           Purchase - To add an available item to the cart 
+           Remove - To remove item from cart
+           Checkout - To purchase items
+           ''')
+    option = input("What do you want to do, " + x.title() + " " )
+    user = Bank(x,age)
+    if option.lower() == "details":
+        user.displaydetails()
+        return ui (x,age)
+    elif option.lower() == "deposit":
+        amount = int(input("How much do we you want to deposit? "))
+        if amount < 0:
+            print("You can not deposit negative money")
+            return ui (x,age)
+        else: 
+            user.deposit(amount)
     elif option.lower() == "withdraw":
         pass
     elif option.lower() == "display":
