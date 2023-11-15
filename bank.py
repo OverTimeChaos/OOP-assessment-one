9084b053a4d88f38038a3229b93a972ac7fe9359
OverTimeChaos <jamesfan2006@hotmail.com>
2023-11-15 22:54:43 +1100

    Finished bank methods, added comments, squished bugs

--- bank.py
+++ bank.py
@@ -5,17 +5,73 @@ class User:
         self.age = age
     def displaydetails(self):
         #displays details of account
-        print ("Name: " + self.name)
-        print ("Age: " + self.age)
+        print ("Name: " + self.name.title())
+        print ("Age: " + str(self.age))
 class Bank (User):
     def __init__(self,name,age):
-        super().__init__ (name,age)
-        self.balance = 0 
+        #allows perimeters to be inherited also 
+        super().__init__ (name,age) 
+        self.balance = 0
     def deposit (self,amount):
+        #allows user to add money to account
         self.balance = self.balance + amount
         print (f"New balance is {self.balance}")
-def ui (x,age):
-    print ('''Enter:
+    def withdraw (self,amount):
+        #allows user to redraw money from account
+        self.balance = self.balance - amount
+    def displaybank (self):
+        # display balance
+        print ("Balance: " + str(self.balance))
+def ui (x,age,option,user):
+    if option.lower() == "details":
+        user.displaydetails()
+        return options (x,age,user)
+    elif option.lower() == "deposit":
+        try:
+            #checks if the value entered is a integer
+            amount = int(input("How much do we you want to deposit? "))
+            if amount < 0:
+                #checks if the value is above zero and is not negative
+                print("You can not deposit negative money")
+                return ui (x,age,option,user)
+            else: 
+                user.deposit(amount)
+                return options (x,age,user)
+        except ValueError:
+            print ("please enter a valid \033[1mNUMBER\033[0m")
+            return ui (x,age,option,user)
+    elif option.lower() == "withdraw":
+        try:
+            #checks if the value entered is a integer
+            amount = int(input("How much do we you want to deposit? "))
+            if amount < 0:
+                #checks if the value is above zero and is not negative
+                print("You can not withdraw negative money")
+                return ui (x,age,option,user)
+            elif user.balance < amount:
+                #checks if the user had enough money to withdraw
+                print ("You can not withdraw that amount of money")
+                return ui(x,age,option,user)
+            else: 
+                user.withdraw(amount)
+                return options (x,age,user)
+        except ValueError:
+            print ("please enter a valid \033[1m   NUMBER \033[0m")
+            return options(x,age,user)
+    elif option.lower() == "display":
+        user.displaybank()
+        return options(x,age,user)
+    elif option.lower() == "buy":
+        pass
+    elif option.lower() == "remove from cart":
+        pass
+    elif option.lower() == "checkout":
+        pass
+    else:
+        print ("Please enter valid option")
+        return options(x,age)
+def options(x,age,user):
+    print ('''\033[1mEnter:\033[0m
         \033[1m   Account \033[0m
            Details - TO display account details
         \033[1m   Bank \033[0m
@@ -28,33 +84,12 @@ def ui (x,age):
            Checkout - To purchase items
            ''')
     option = input("What do you want to do, " + x.title() + " " )
-    user = Bank(x,age)
-    if option.lower() == "details":
-        user.displaydetails()
-        return ui (x,age)
-    elif option.lower() == "deposit":
-        amount = int(input("How much do we you want to deposit? "))
-        if amount < 0:
-            print("You can not deposit negative money")
-            return ui (x,age)
-        else: 
-            user.deposit(amount)
-    elif option.lower() == "withdraw":
-        pass
-    elif option.lower() == "display":
-        pass
-    elif option.lower() == "buy":
-        pass
-    elif option.lower() == "remove from cart":
-        pass
-    elif option.lower() == "checkout":
-        pass
-    else:
-        print ("Please enter valid option")
-        return ui(x,age)
+    ui (x,age,option,user)
+    
 
 
 def logon(x):
+    
     global lockingout
     if lockingout == 3:
         print ("Failed too many times. You have been locked out.")
@@ -63,19 +98,25 @@ def logon(x):
         if x.lower() == "aidan":
             print ("Welcome, Aidan")
             age = -127
-            ui(x,age)
+            user = Bank(x,age)
+            options(x,age,user)
         elif x.lower() == "akhil":
             print ("Welcome, Akhil")
             age = 17
-            ui(x,age)
+            user = Bank(x,age)
+            options(x,age,user)
         elif x.lower() == "adam":
             print ("Welcome, Adam")
             age = 1
-            ui(x,age)
+            user = Bank(x,age)
+            options(x,age,user)
+            
             
         else:
             print ("You have failed to enter a valid user")
             lockingout = lockingout + 1
             return logon(input ("please enter a name "))
 lockingout = 0
-logon(input ("Please enter a name "))
+
+startuser = input ("Please enter a name ")
+logon(startuser)
